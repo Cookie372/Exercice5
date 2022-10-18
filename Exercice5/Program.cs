8113e2a731326aa22d3d16155c7e2e259cda0cd5
Utilisateur7.da <Utilisateur7.da@CCT-0621-LN-002>
2022-10-18 15:54:29 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre âge :");
+            int âge = int.Parse(Console.ReadLine());
+            if (âge >= 18)
+            {
+                Console.WriteLine("Vous êtes majeur");
+            }
+            else
+            {
+                Console.WriteLine("Vous êtes mineur(e)");
+                Console.WriteLine("Vous serez majeur dans " + (18 - âge) + " an(s)");
+            }
+            Console.ReadLine();
+        }
+    }
+}
